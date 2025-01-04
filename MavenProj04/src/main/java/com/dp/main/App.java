4355288ea0666aa3ec744c7019dba6d662dd4926
Dharani2018 <dharaniparimella13@gmail.com>
2025-01-04 15:30:27 -0800

    add method added by TL

--- MavenProj04/src/main/java/com/dp/main/App.java
+++ MavenProj04/src/main/java/com/dp/main/App.java
@@ -6,6 +6,9 @@ package com.dp.main;
  */
 public class App 
 {
+	public void add(int x, int y) {
+		System.out.println(x+y);
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
