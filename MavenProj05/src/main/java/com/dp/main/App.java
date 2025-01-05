38c6d29fb5428a4c47eddb91d5b7bb9cd5980f8f
Dharani2018 <dharaniparimella13@gmail.com>
2025-01-05 12:14:34 -0800

    Added add() method by Dev1.

--- MavenProj05/src/main/java/com/dp/main/App.java
+++ MavenProj05/src/main/java/com/dp/main/App.java
@@ -6,6 +6,9 @@ package com.dp.main;
  */
 public class App 
 {
+	public void add(int a,int b) {
+		System.out.println("add "+a+b);
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
