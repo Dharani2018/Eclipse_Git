1f4bc0de53dc165eb7078190d409803c1d7b0b7b
Dharani2018 <dharaniparimella13@gmail.com>
2025-01-05 12:17:55 -0800

    sub() added method by TL.

--- MavenProj05/src/main/java/com/dp/main/App.java
+++ MavenProj05/src/main/java/com/dp/main/App.java
@@ -9,6 +9,9 @@ public class App
 	public void add(int a,int b) {
 		System.out.println("add "+a+b);
 	}
+	public void sub(int a, int b) {
+		System.out.println("sub "+(a-b));
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
